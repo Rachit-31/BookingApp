812751e7e88bcdf73575bfd3db5d893c648c612d
Rachit Jain <rachitjainjaipur@gmail.com>
2024-05-30 20:39:06 +0530

    .

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-
+import { FaPaperPlane } from "react-icons/fa";
 function App() {
   return (
-    <>
-          <header className="bg-blue-500 text-white p-4">
-        <h1 className="text-3xl font-bold">Welcome to My Tailwind CSS App</h1>
+    <div>
+      <header className='p-4'>
+        <a href="" className='flex items-center gap-1'><FaPaperPlane /> <span className='font-bold text-xl'>Bookify</span></a>
       </header>
-    </>
+    </div>
   )
 }
 
